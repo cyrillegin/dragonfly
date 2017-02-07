e929aa8e8001d92b3816b5ee975989c5c19e82ec
Cyrille Gindreau <cyrillegin@gmail.com>
2017-02-06 19:05:28 -0700

    changed varible names

--- aquaponicMCP/aquaponicMCP.ino
+++ aquaponicMCP/aquaponicMCP.ino
@@ -1,47 +1,45 @@
 
 #include <OneWire.h>
-//L = left sensor, R = right sensor
-#define lightSensorR A0
-#define lightSensorL A1
+#define plantLight A0
+#define aquaLight A1
 OneWire  ds(2); //water temp sensor on digital pin 2
 #define waterTurbSensor A3
 
-#define minLightL 0
-#define minLightR 0
+#define minLightPlant 0
+#define minLightAqua 0
 #define targetTemperature 0
 #define temperatureVarience 0
 #define targetTurbidity 0
 #define turbidityVarience 0
 
-#define lLightLed 13
-#define rLightLed 12
+#define plantLed 13
+#define aquaLed 12
 #define turbLed 11
 #define tempLed 10
 
-float lightR = 0.0;
-float lightL = 0.0;
-float waterTemp = 0.0;
-float waterTurb = 0.0;
+float aquaLightReading = 0.0;
+float plantLightReading = 0.0;
+float waterTempReading = 0.0;
+float waterTurbReading = 0.0;
 
-uint16_t lightRBuffer[64];
-uint16_t lightLBuffer[64];
+uint16_t plantLightBuffer[64];
+uint16_t aquaLightBuffer[64];
 uint16_t tempBuffer[64];
 uint16_t turbBuffer[64];
 
 uint8_t bufferCounter = 1;
 
-bool leftLightOn = false;
-bool rightLightOn = false;
-bool aquariumLightOn = false;
+bool plantLightOn = false;
+bool aquaLightOn = false;
 
 void setup() {
   Serial.begin(9600);
-  lightRBuffer[0] = 1791;
-  lightLBuffer[0] = 1792;
+  plantLightBuffer[0] = 1791;
+  aquaLightBuffer[0] = 1792;
   tempBuffer[0] = 1793;
   turbBuffer[0] = 1794;
-  pinMode(lLightLed, OUTPUT);
-  pinMode(rLightLed, OUTPUT);
+  pinMode(plantLed, OUTPUT);
+  pinMode(aquaLed, OUTPUT);
   pinMode(turbLed, OUTPUT);
   pinMode(tempLed, OUTPUT);
 }
@@ -59,23 +57,23 @@ void loop() {
  */
 void LogData(){
   if(bufferCounter >= 64) return;
-  lightR = analogRead(lightSensorR);
-  lightL = analogRead(lightSensorL);
-  waterTemp = analogRead(waterTempSensor);
-  waterTurb = analogRead(waterTurbSensor);
-  lightRBuffer[bufferCounter] = lightR;
-  lightLBuffer[bufferCounter] = lightL;
-  tempBuffer[bufferCounter] = waterTemp;
-  turbBuffer[bufferCounter] = waterTurb;
+  plantLightReading = analogRead(aquaLight);
+  aquaLightReading = analogRead(plantLight);
+//  waterTempReading = analogRead(waterTempSensor);
+  waterTurbReading = analogRead(waterTurbSensor);
+  plantLightBuffer[bufferCounter] = plantLightReading;
+  aquaLightBuffer[bufferCounter] = aquaLightReading;
+  tempBuffer[bufferCounter] = waterTempReading;
+  turbBuffer[bufferCounter] = waterTurbReading;
   bufferCounter += 1;
-  Serial.print("leftLight: ");
-  Serial.print(lightL);
+  Serial.print("aquaLight: ");
+  Serial.print(aquaLightReading);
   Serial.print(" rightLight: ");
-  Serial.print(lightR);
+  Serial.print(plantLightReading);
   Serial.print(" waterTemp: ");
-  Serial.println(waterTemp);
+  Serial.println(waterTempReading);
   Serial.print(" waterTurb: ");
-  Serial.print(waterTurb);
+  Serial.print(waterTurbReading);
 }
 
 //im sure there is a better way, consider a timeout? 
@@ -83,8 +81,8 @@ void LogData(){
 //because this might take to long
 void SendData(){
   if(bufferCounter >= 64){
-    SendBuffer(lightRBuffer);
-    SendBuffer(lightLBuffer);
+    SendBuffer(plantLightBuffer);
+    SendBuffer(aquaLightBuffer);
     SendBuffer(tempBuffer);
     SendBuffer(turbBuffer);
     bufferCounter = 1;
@@ -109,21 +107,21 @@ void GetInput(){
 void SendCommands(){
 //  sensor operation
 //  lighting
-  if(leftLightOn && lightL < minLightL){
-    digitalWrite(lLightLed, HIGH);
+  if(aquaLightOn && aquaLightReading < minLightAqua){
+    digitalWrite(aquaLed, HIGH);
   }
-  if(rightLightOn && lightR < minLightR){
-    digitalWrite(rLightLed, HIGH);
+  if(plantLightOn && plantLightReading < minLightPlant){
+    digitalWrite(plantLed, HIGH);
   }
   
   //water temperature
-  float tempDifference = targetTemperature > waterTemp ? targetTemperature - waterTemp : waterTemp - targetTemperature;
+  float tempDifference = targetTemperature > waterTempReading ? targetTemperature - waterTempReading : waterTempReading - targetTemperature;
   if(tempDifference > temperatureVarience){
     digitalWrite(tempLed, HIGH);
   }
 
   //water turbidity
-  float turbDifference = targetTurbidity > waterTurb ? targetTurbidity - waterTurb : waterTurb - targetTurbidity;
+  float turbDifference = targetTurbidity > waterTurbReading ? targetTurbidity - waterTurbReading : waterTurbReading - targetTurbidity;
   if(turbDifference > turbidityVarience){
     digitalWrite(turbLed, HIGH);
   }
