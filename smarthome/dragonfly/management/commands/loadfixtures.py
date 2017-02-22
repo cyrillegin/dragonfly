52b5fb786b858c4cc36758b480899a2ba1f043d1
Cyrille Gindreau <cyrillegin@gmail.com>
2017-02-22 10:43:32 -0700

    updated to include light switch

--- smarthome/dragonfly/management/commands/loadfixtures.py
+++ smarthome/dragonfly/management/commands/loadfixtures.py
@@ -8,11 +8,35 @@ class Command(BaseCommand):
     help = 'Load a days worth of data.'
 
     def handle(self, *args, **options):
-        testSense = models.Sensor(name="test1", description="A test sensor", coefficients="(1,0)")
-        testSense.save()
+        testTemp = models.Sensor(name="test1", description="A test sensor", coefficients="(1,0)", sensor_type="temperature")
+        testTemp.save()
 
         for i in range(0, 24 * 60):
-            newVal = 400 * math.sin(0.1*i)
+            newVal = 60 + 30 * math.sin(0.1 * i)
             print "saving: {}".format(newVal)
-            newReading = models.Reading(sensor=testSense, value=newVal)
+            newReading = models.Reading(sensor=testTemp, value=newVal)
+            newReading.save()
+
+        testCleanliness = models.Sensor(name="test1", description="A test sensor", coefficients="(1,0)", sensor_type="cleanliness")
+        testCleanliness.save()
+
+        for i in range(0, 24 * 60):
+            newVal = 50 + 50 * math.sin(0.1 * i)
+            print "saving: {}".format(newVal)
+            newReading = models.Reading(sensor=testCleanliness, value=newVal)
+            newReading.save()
+
+        testlightSensor = models.Sensor(name="test1", description="A test sensor", coefficients="(1,0)", sensor_type="lightsensor")
+        testlightSensor.save()
+
+        for i in range(0, 24 * 60):
+            newVal = 500 + 500 * math.sin(0.1 * i)
+            print "saving: {}".format(newVal)
+            newReading = models.Reading(sensor=testlightSensor, value=newVal)
+            newReading.save()
+
+        for i in range(0, 24 * 60):
+            newVal = int(math.sin(0.1 * i) + 0.5)
+            print "saving: {}".format(newVal)
+            newReading = models.Reading(sensor=testlightSensor, value=newVal)
             newReading.save()
