60d4de31a1deb0d6eb104c475371d1eecc5042c7
Cyrille Gindreau <cyrillegin@gmail.com>
2017-03-06 12:38:09 -0700

    added checks to make sure that lights are either on or off and displays time when changing.

--- smarthome/dragonfly/management/commands/automate.py
+++ smarthome/dragonfly/management/commands/automate.py
@@ -3,12 +3,14 @@ from datetime import datetime
 import json
 import time
 
+from dragonfly import models
+
 
 class Command(BaseCommand):
     help = 'Load a days worth of data.'
 
     def handle(self, *args, **options):
-        TurnOnTime = 6
+        TurnOnTime = 8
         TurnOffTime = 20
         IsOn = True
         # 60 seconds * 30 minutes = check every half hour
@@ -17,13 +19,28 @@ class Command(BaseCommand):
         while(True):
             currentHour = datetime.now().hour
             if IsOn is True and TurnOffTime is currentHour:
-                print "Turning off lights"
+                print "Turning off lights, the current time is {}".format(datetime.now())
                 with open('commandQueue.json', 'w') as outfile:
                     json.dump({'value': 0}, outfile)
                 IsOn = False
             if IsOn is False and TurnOnTime is currentHour:
-                print "Turing on lights"
+                print "Turing on lights, the current time is {}".format(datetime.now())
                 with open('commandQueue.json', 'w') as outfile:
                     json.dump({'value': 1}, outfile)
                 IsOn = True
-            time.sleep(CheckRate)
+            time.sleep(CheckRate / 2)
+            currentHour = datetime.now().hour
+            if currentHour > TurnOnTime and currentHour < TurnOffTime:
+                # light is on
+                switch = models.Sensor.objects.get(name="lightSwitch")
+                if switch.toDict().isOn is False:
+                    with open('commandQueue.json', 'w') as outfile:
+                        json.dump({'value': 1}, outfile)
+
+            else:
+                # light is off
+                switch = models.Sensor.objects.get(name="lightSwitch")
+                if switch.toDict().isOn is True:
+                    with open('commandQueue.json', 'w') as outfile:
+                        json.dump({'value': 0}, outfile)
+            time.sleep(CheckRate / 2)
