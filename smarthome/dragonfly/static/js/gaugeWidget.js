94e717807f4b01ea302c240617c93447510fd26c
Cyrille Gindreau <cyrillegin@gmail.com>
2017-03-16 17:03:21 -0600

    formatting js

--- smarthome/dragonfly/static/js/gaugeWidget.js
+++ smarthome/dragonfly/static/js/gaugeWidget.js
@@ -11,7 +11,6 @@ angular.module('dragonfly.gaugecontroller', [])
   }, function(v){
     if(v === undefined) return;
     for( var i in v){
-      // console.log(v[i])
       $scope.gauges.push({'id': 'gaugeChart-'+i})
     }
 
@@ -23,7 +22,6 @@ angular.module('dragonfly.gaugecontroller', [])
   });
 
   function DrawTempChart(data, id){
-    // if(data.lastReading === 0) return;
     var config = {
       size: 90,
       label: data.name,
@@ -51,26 +49,26 @@ angular.module('dragonfly.gaugecontroller', [])
     config.transitionDuration = 500;
   
     var svg = d3.select("#gaugeChart-"+id)
-              .append("svg:svg")
-              .attr("class", "gauge")
-              .attr("width", config.size)
-              .attr("height", config.size);
+        .append("svg:svg")
+        .attr("class", "gauge")
+        .attr("width", config.size)
+        .attr("height", config.size);
     
     svg.append("svg:circle")
-          .attr("cx", config.cx)
-          .attr("cy", config.cy)
-          .attr("r", config.raduis)
-          .style("fill", "#ccc")
-          .style("stroke", "#000")
-          .style("stroke-width", "0.5px");
+        .attr("cx", config.cx)
+        .attr("cy", config.cy)
+        .attr("r", config.raduis)
+        .style("fill", "#ccc")
+        .style("stroke", "#000")
+        .style("stroke-width", "0.5px");
           
     svg.append("svg:circle")
-          .attr("cx", config.cx)
-          .attr("cy", config.cy)
-          .attr("r", 0.9 * config.raduis)
-          .style("fill", "#fff")
-          .style("stroke", "#e0e0e0")
-          .style("stroke-width", "2px");
+        .attr("cx", config.cx)
+        .attr("cy", config.cy)
+        .attr("r", 0.9 * config.raduis)
+        .style("fill", "#fff")
+        .style("stroke", "#e0e0e0")
+        .style("stroke-width", "2px");
           
     for (var index in config.greenZones){
       drawBand(config.greenZones[index].from, config.greenZones[index].to, config.greenColor);
@@ -87,14 +85,14 @@ angular.module('dragonfly.gaugecontroller', [])
     if (undefined != config.label){
       var fontSize = Math.round(config.size / 9);
       svg.append("svg:text")
-            .attr("x", config.cx)
-            .attr("y", config.cy / 2 + fontSize / 2)
-            .attr("dy", fontSize / 2)
-            .attr("text-anchor", "middle")
-            .text(config.label)
-            .style("font-size", fontSize + "px")
-            .style("fill", "#333")
-            .style("stroke-width", "0px");
+          .attr("x", config.cx)
+          .attr("y", config.cy / 2 + fontSize / 2)
+          .attr("dy", fontSize / 2)
+          .attr("text-anchor", "middle")
+          .text(config.label)
+          .style("font-size", fontSize + "px")
+          .style("fill", "#333")
+          .style("stroke-width", "0px");
     }
     
     var fontSize = Math.round(config.size / 16);
@@ -106,37 +104,37 @@ angular.module('dragonfly.gaugecontroller', [])
         var point2 = valueToPoint(minor, 0.85);
         
         svg.append("svg:line")
-              .attr("x1", point1.x)
-              .attr("y1", point1.y)
-              .attr("x2", point2.x)
-              .attr("y2", point2.y)
-              .style("stroke", "#666")
-              .style("stroke-width", "1px");
+            .attr("x1", point1.x)
+            .attr("y1", point1.y)
+            .attr("x2", point2.x)
+            .attr("y2", point2.y)
+            .style("stroke", "#666")
+            .style("stroke-width", "1px");
       }
       
       var point1 = valueToPoint(major, 0.7);
       var point2 = valueToPoint(major, 0.85);  
       
       svg.append("svg:line")
-            .attr("x1", point1.x)
-            .attr("y1", point1.y)
-            .attr("x2", point2.x)
-            .attr("y2", point2.y)
-            .style("stroke", "#333")
-            .style("stroke-width", "2px");
+          .attr("x1", point1.x)
+          .attr("y1", point1.y)
+          .attr("x2", point2.x)
+          .attr("y2", point2.y)
+          .style("stroke", "#333")
+          .style("stroke-width", "2px");
       
       if (major == config.min || major == config.max){
         var point = valueToPoint(major, 0.63);
         
         svg.append("svg:text")
-              .attr("x", point.x)
-              .attr("y", point.y)
-              .attr("dy", fontSize / 3)
-              .attr("text-anchor", major == config.min ? "start" : "end")
-              .text(major)
-              .style("font-size", fontSize + "px")
-              .style("fill", "#333")
-              .style("stroke-width", "0px");
+            .attr("x", point.x)
+            .attr("y", point.y)
+            .attr("dy", fontSize / 3)
+            .attr("text-anchor", major == config.min ? "start" : "end")
+            .text(major)
+            .style("font-size", fontSize + "px")
+            .style("fill", "#333")
+            .style("stroke-width", "0px");
       }
     }
     
@@ -147,124 +145,114 @@ angular.module('dragonfly.gaugecontroller', [])
     var pointerPath = buildPointerPath(midValue);
     
     var pointerLine = d3.line()
-                  .x(function(d) { return d.x })
-                  .y(function(d) { return d.y })
-                  .curve(d3.curveBasis);
+        .x(function(d) { return d.x })
+        .y(function(d) { return d.y })
+        .curve(d3.curveBasis);
     
     pointerContainer.selectAll("path")
-              .data([pointerPath])
-              .enter()
-                .append("svg:path")
-                  .attr("d", pointerLine)
-                  .style("fill", "#dc3912")
-                  .style("stroke", "#c63310")
-                  .style("fill-opacity", 0.7)
+        .data([pointerPath])
+        .enter()
+        .append("svg:path")
+            .attr("d", pointerLine)
+            .style("fill", "#dc3912")
+            .style("stroke", "#c63310")
+            .style("fill-opacity", 0.7)
           
     pointerContainer.append("svg:circle")
-              .attr("cx", config.cx)
-              .attr("cy", config.cy)
-              .attr("r", 0.12 * config.raduis)
-              .style("fill", "#4684EE")
-              .style("stroke", "#666")
-              .style("opacity", 1);
+        .attr("cx", config.cx)
+        .attr("cy", config.cy)
+        .attr("r", 0.12 * config.raduis)
+        .style("fill", "#4684EE")
+        .style("stroke", "#666")
+        .style("opacity", 1);
     
     var fontSize = Math.round(config.size / 10);
     pointerContainer.selectAll("text")
-              .data([midValue])
-              .enter()
-                .append("svg:text")
-                  .attr("x", config.cx)
-                  .attr("y", config.size - config.cy / 4 - fontSize)
-                  .attr("dy", fontSize / 2)
-                  .attr("text-anchor", "middle")
-                  .style("font-size", fontSize + "px")
-                  .style("fill", "#000")
-                  .style("stroke-width", "0px");
-
-    //Put the value here, make sure to subtract 50(for now)
+        .data([midValue])
+        .enter()
+        .append("svg:text")
+            .attr("x", config.cx)
+            .attr("y", config.size - config.cy / 4 - fontSize)
+            .attr("dy", fontSize / 2)
+            .attr("text-anchor", "middle")
+            .style("font-size", fontSize + "px")
+            .style("fill", "#000")
+            .style("stroke-width", "0px");
 
     var val = data.lastReading;
     redraw(svg, val);
   
-  function buildPointerPath(value){
-    var delta = config.range / 13;
+    function buildPointerPath(value){
+      var delta = config.range / 13;
 
-    var head = valueToPoint(value - majorDelta, 0.85);
-    var head1 = valueToPoint(value - delta, 0.12);
-    var head2 = valueToPoint(value + delta, 0.12);
+      var head = valueToPoint(value - majorDelta, 0.85);
+      var head1 = valueToPoint(value - delta, 0.12);
+      var head2 = valueToPoint(value + delta, 0.12);
 
-    
-    var tailValue = value - (config.range * (1/(270/360)) / 2);
-    var tail = valueToPoint(tailValue, 0.28);
-    var tail1 = valueToPoint(tailValue - delta, 0.12);
-    var tail2 = valueToPoint(tailValue + delta, 0.12);
-    
-    return [head, head1, tail2, tail, tail1, head2, head];
-    
-    function valueToPoint(value, factor){
-      var point = {'x': 0, 'y':0}  
-      point.x += value*factor;
-      point.y += value*factor;
-      return point;
+      
+      var tailValue = value - (config.range * (1/(270/360)) / 2);
+      var tail = valueToPoint(tailValue, 0.28);
+      var tail1 = valueToPoint(tailValue - delta, 0.12);
+      var tail2 = valueToPoint(tailValue + delta, 0.12);
+      
+      return [head, head1, tail2, tail, tail1, head2, head];
+      
+      function valueToPoint(value, factor){
+        var point = {'x': 0, 'y':0}  
+        point.x += value*factor;
+        point.y += value*factor;
+        return point;
+      }
     }
-  }
   
-  function drawBand(start, end, color){
-    if (0 >= end - start) return;
-    
-    svg.append("svg:path")
-          .style("fill", color)
-          .attr("d", d3.arc()
-            .startAngle(valueToRadians(start))
-            .endAngle(valueToRadians(end))
-            .innerRadius(0.65 * config.raduis)
-            .outerRadius(0.85 * config.raduis))
+    function drawBand(start, end, color){
+      if (0 >= end - start) return;
+      
+      svg.append("svg:path")
+        .style("fill", color)
+        .attr("d", d3.arc()
+          .startAngle(valueToRadians(start))
+          .endAngle(valueToRadians(end))
+          .innerRadius(0.65 * config.raduis)
+          .outerRadius(0.85 * config.raduis))
           .attr("transform", function() { return "translate(" + config.cx + ", " + config.cy + ") rotate(270)" });
-  }
+    }
   
-  function redraw(svg, value, transitionDuration){
-    var pointerContainer = svg.select(".pointerContainer");
-    
-    pointerContainer.selectAll("text").text(Math.round(value));
-    
-    var pointer = pointerContainer.selectAll("path");
-    pointer.transition()
+    function redraw(svg, value, transitionDuration){
+      var pointerContainer = svg.select(".pointerContainer");
+      
+      pointerContainer.selectAll("text").text(Math.round(value));
+      
+      var pointer = pointerContainer.selectAll("path");
+      pointer.transition()
           .duration(undefined != transitionDuration ? transitionDuration : config.transitionDuration) 
           .attrTween("transform", function(){
-            var pointerValue = value;
-            
-            if (value > config.max) pointerValue = config.max + 0.02*config.range;
-            else if (value < config.min) pointerValue = config.min - 0.02*config.range;
-            var targetRotation = (valueToDegrees(pointerValue) - 90);
-            var currentRotation = self._currentRotation || targetRotation;
-            self._currentRotation = targetRotation;
-            
-            return function(step) {
-              var rotation = currentRotation + (targetRotation-currentRotation)*step;
-              console.log(rotation)
-              return "translate(" + config.cx + ", " + config.cy + ") rotate(" + (225+rotation) + ")"; 
-            }
+              var pointerValue = value;
+              
+              if (value > config.max) pointerValue = config.max + 0.02*config.range;
+              else if (value < config.min) pointerValue = config.min - 0.02*config.range;
+              var targetRotation = (valueToDegrees(pointerValue) - 90);
+              var currentRotation = self._currentRotation || targetRotation;
+              self._currentRotation = targetRotation;
+              
+              return function(step) {
+                  var rotation = currentRotation + (targetRotation-currentRotation)*step;
+                  return "translate(" + config.cx + ", " + config.cy + ") rotate(" + (225+rotation) + ")"; 
+              }
           });
+    }
+  
+    function valueToDegrees(value){
+      return value / config.range * 270 - (config.min / config.range * 270 + 45);
+    }
+    
+    function valueToRadians(value){
+      return valueToDegrees(value) * Math.PI / 180;
+    }
+    
+    function valueToPoint(value, factor){
+      return {  x: config.cx - config.raduis * factor * Math.cos(valueToRadians(value)),
+            y: config.cy - config.raduis * factor * Math.sin(valueToRadians(value))    };
+    }
   }
-  
-  function valueToDegrees(value){
-    // thanks @closealert
-    //return value / this.config.range * 270 - 45;
-    return value / config.range * 270 - (config.min / config.range * 270 + 45);
-  }
-  
-  function valueToRadians(value){
-    return valueToDegrees(value) * Math.PI / 180;
-  }
-  
-  function valueToPoint(value, factor){
-    return {  x: config.cx - config.raduis * factor * Math.cos(valueToRadians(value)),
-          y: config.cy - config.raduis * factor * Math.sin(valueToRadians(value))    };
-  }
-  
-   
-}
-
-
-  
 }]);
