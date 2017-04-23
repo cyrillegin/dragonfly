a5e68e924e7ceac78ceb4586433bd1fb2c36ede1
Cyrille Gindreau <cyrillegin@gmail.com>
2017-04-23 14:18:41 -0600

    backup command now working.

--- commands/databaseCommands.py
+++ commands/databaseCommands.py
@@ -1,6 +1,16 @@
 import json
 import requests
 import os
+import time
+from multiprocessing import Process
+from datetime import date
+from sqlalchemy import create_engine
+# import Aquaponics
+from sessionManager import sessionScope
+from models import Sensor, Reading, Log
+import models
+from shutil import copyfile
+
 SENSORURL = "https://dragonf1y.herokuapp.com/api/sensor"
 READINGURL = "https://dragonf1y.herokuapp.com/api/reading"
 
@@ -35,16 +45,21 @@ dragonfly_database_backup.DD-MM-YY
 
 def BackupDatabase():
     backups = []
-    startTime = 0
+    
     lastBackup = None
     lastDate = [0, 0, 0]
+    currentDate = str(date.today()).split('-')
     for dirpath, dirnames, filenames in os.walk('backups/'):
         backups = filenames
     if len(backups) == 0:
         print "No previous databases found, starting from the begining of time."
+        currentBackup = "backups/dragonfly_database_backup.{}-{}-{}.db".format(currentDate[2], currentDate[1], currentDate[0])
+        lastDate = currentDate.reverse()
     else:
-
+        print backups
         for i in backups:
+            if i.startswith('.'):
+                continue
             dates = i.split('.')[1].split('-')
             if int(dates[2]) > int(lastDate[2]):
                 lastDate = dates
@@ -57,8 +72,57 @@ def BackupDatabase():
                     if int(dates[0]) > int(lastDate[0]):
                         lastDate = dates
                         lastBackup = i
-        print lastDate
-        print lastBackup
+        print "Creating copy"
+        src = os.path.join(dirpath, lastBackup)
+        dest = "backups/dragonfly_database_backup.{}-{}-{}.db".format(currentDate[2], currentDate[1], currentDate[0])
+        copyfile(src, dest)
+        currentBackup = dest
+
+    print "opening database"
+    dbURL = "sqlite:///{}".format(currentBackup)
+    with sessionScope(dbURL) as session:
+        try:
+            lastReading = session.query(Reading).order_by(Reading.created.desc()).first()
+            print lastReading.toDict()
+            startTime = lastReading.toDict()['created']
+        except:
+            print 'database was empty, setting time to 0.'
+            startTime = 0
+        print 'making request to api with start time: {}'.format(startTime)
+
+        print "getting sensors"
+        newSensors = requests.get(SENSORURL)
+        print newSensors.json()
+        for i in newSensors.json()['sensor_list']:
+            try:
+                newSensor = session.query(Sensor).filter_by(name=i['name']).one()
+                print "sensor already exists: {}".format(i['name'])
+            except:
+                print "Sensor doesn't yet exist, adding: {}".format(i['name'])
+                newSensor = Sensor(name=i['name'])
+                for j in i:
+                    if j != 'name':
+                        setattr(newSensor, j, i[j])
+                session.add(newSensor)
+
+            print "Getting readings for {}".format(i['name'])
+            url = "{}?sensor={}&start={}&end={}".format(READINGURL, i['name'], int(startTime), int(time.time()))
+            print url
+            newReadings = requests.get(url)
+            for j in newReadings.json()['readings']:
+                newReading = Reading(created=j['created'], sensor=newSensor.toDict()['name'], value=j['value'])
+                session.add(newReading)
+        print "committing"
+        session.commit()
+        print 'all done!'
+
+
+
+    # p = Process(target=MakeRequest, args=(j, ))
+
+
+def MakeRequest(session, url):
+    pass
 
 
 
