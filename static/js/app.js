f3da1981ab813175324cc520b7185ee262910202
Cyrille Gindreau <cyrillegin@gmail.com>
2017-06-11 15:30:01 -0600

    prettify js, add hint and lint

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,17 @@
 /*jslint node: true */
 'use strict';
 
-var angular;
-
 // Declare app level module which depends on views, and components
 var app = angular.module('dragonfly', [
-  'ngRoute',
-  'dragonfly.maincontroller',
-  'dragonfly.gaugecontroller',
-  'dragonfly.treecontroller',
-  'dragonfly.graphcontroller',
-  'dragonfly.services'
+    'ngRoute',
+    'dragonfly.maincontroller',
+    'dragonfly.gaugecontroller',
+    'dragonfly.treecontroller',
+    'dragonfly.graphcontroller',
+    'dragonfly.services'
 ])
 
-.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider){
+.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
     // $routeProvider.otherwise({redirectTo: '/'});
     $routeProvider.when('/', {
         templateUrl: '/templates/base.html',
