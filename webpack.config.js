2e621844d0c45daca3f64cd9234b3e1b02f6498b
Cyrille Gindreau <cyrillegin@gmail.com>
2017-08-05 14:33:39 -0600

    creating production plugin set

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,62 @@ const BundleTracker = require('webpack-bundle-tracker');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const Production = true;
+
+const plugins = [
+    new BundleTracker({
+        filename: './webpack-stats.json',
+    }),
+
+    new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        jquery: 'jquery',
+    }),
+    new BundleAnalyzerPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.bundle.js',
+    }),
+    new webpack.NamedModulesPlugin(),
+];
+
+if (Production) {
+    console.log('Building for production');
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        compress: {
+            warnings: false,
+            screw_ie8: true,
+            conditionals: true,
+            unused: true,
+            comparisons: true,
+            sequences: true,
+            dead_code: true,
+            drop_debugger: true,
+            drop_console: true,
+            evaluate: true,
+            if_return: true,
+            join_vars: true,
+        },
+        output: {
+            comments: false,
+        },
+    }));
+    plugins.push(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+    }));
+
+    plugins.push(new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production'),
+        },
+    }));
+} else {
+    console.log('Building for development.');
+}
+
 module.exports = {
     entry: {
         app: './src/app.js',
@@ -18,7 +74,7 @@ module.exports = {
             './node_modules/bootstrap/dist/css/bootstrap.min.css',
             './node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css',
             './node_modules/bootstrap-switch/dist/js/bootstrap-switch.min.js',
-            './node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css'
+            './node_modules/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
         ],
     },
     output: {
@@ -63,41 +119,5 @@ module.exports = {
             loader: 'url-loader',
         }],
     },
-    plugins: [
-        new BundleTracker({
-            filename: './webpack-stats.json',
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true,
-            debug: false,
-        }),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false,
-        //         screw_ie8: true,
-        //         conditionals: true,
-        //         unused: true,
-        //         comparisons: true,
-        //         sequences: true,
-        //         dead_code: true,
-        //         evaluate: true,
-        //         if_return: true,
-        //         join_vars: true,
-        //     },
-        //     output: {
-        //         comments: false
-        //     },
-        // }),
-        new webpack.ProvidePlugin({
-            jQuery: 'jquery',
-            $: 'jquery',
-            jquery: 'jquery',
-        }),
-        new BundleAnalyzerPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'vendor.bundle.js',
-        }),
-        new webpack.NamedModulesPlugin(),
-    ]
-}
+    plugins: plugins,
+};
