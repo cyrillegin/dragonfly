c7a2e04aa211c204ca4e8e6246e1f2264ab3798e
Cyrille Gindreau <cyrillegin@gmail.com>
2018-01-21 08:37:50 -0700

    adding config file

---
+++ config-template.py
@@ -0,0 +1,12 @@
+"""
+config.py
+
+Holds config information for each system.
+"""
+DBFILE = '/dragonDB.db'
+isMCP = False
+hasCam = False
+# Use 'localhost' for base station, else use the local ip of homebase
+MCPIP = 'localhost'
+MCPPORT = '5000'
+STATIONNAME = 'homebase'
