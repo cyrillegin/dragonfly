f330da8dd53dabf87db46903ae1c48d831c8e884
Cyrille Gindreau <cyrillegin@gmail.com>
2018-07-14 19:07:00 -0600

    adds icons to add sensor list

--- src/client/pages/AddSensor/AddSensor.js
+++ src/client/pages/AddSensor/AddSensor.js
@@ -8,7 +8,9 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/Inbox';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import ACUnitIcon from '@material-ui/icons/AcUnit';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import MessageDialog from './../../components/Dialogs/MessageDialog';
@@ -111,7 +113,6 @@ export class HomePage extends Component {
           meta: this.state.meta,
         },
       ).then((res) => {
-        console.log(res);
         this.setState({
           dialogIsOpen: true,
           dialogMessage: `${res.sensor.name} successfully added using the ${res.sensor.poller} poller.`,
@@ -146,6 +147,24 @@ export class HomePage extends Component {
       });
     };
 
+    const pollerIcon = (icon) => {
+      console.log(icon);
+      switch (icon) {
+        case 'cryptoPoller':
+          return (
+            <MonetizationOnIcon />
+          );
+        case 'gpioPoller':
+          return (
+            <ACUnitIcon />
+          );
+        default:
+          return (
+            <ShowChartIcon />
+          );
+      }
+    };
+
     return (
       <div className={this.props.classes.root}>
 
@@ -171,7 +190,7 @@ export class HomePage extends Component {
                   this.handlePluginSelect(plugin);
                 }}>
                   <ListItemIcon>
-                    <InboxIcon />
+                    {pollerIcon(plugin)}
                   </ListItemIcon>
                   <ListItemText primary={plugin} />
                 </ListItem>
