7b83251c0558f17620aef13bd2d719120255b559
Cyrille Gindreau <cyrillegin@gmail.com>
2018-12-14 15:17:43 -0700

    add manifest and robots

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
 
+document.querySelector('.loader').parentElement.removeChild(document.querySelector('.loader'));
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'), // eslint-disable-line
