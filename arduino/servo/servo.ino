0167403f131fadb554bb0a211dedea79d519844e
Cyrille Gindreau <cyrillegin@gmail.com>
2018-12-22 15:33:41 -0700

    created dummy scripts folder for arduino

---
+++ arduino/servo/servo.ino
@@ -0,0 +1,41 @@
+#include <Servo.h> 
+
+Servo servo; 
+
+int servoPin = 3; 
+int minAngle = 0;
+int maxAngle = 155;
+
+int currentAngle = 90;
+
+void setup() { 
+   servo.attach(servoPin); 
+   Serial.begin(9600);
+}
+
+void loop(){ 
+  
+  String incoming = Serial.readString();
+
+  // Check if number has been recieved.
+  if (incoming.length() == 0) {
+    return;
+  }
+  for(int i = 0; i < incoming.length(); i++) {
+    if (isdigit(incoming[i]) == false) {
+      return;
+    }
+  }
+ 
+  currentAngle = incoming.toInt();
+
+  // Make sure that number is within the bounds of the servo
+  if (currentAngle < minAngle) {
+    currentAngle = minAngle;
+  }
+  if (currentAngle > maxAngle) {
+    currentAngle = maxAngle;
+  }
+
+  servo.write(currentAngle);
+}
