518e6af8d7039a6784919c771521d9f6dad51a50
Cyrille Gindreau <cyrillegin@gmail.com>
2020-05-23 13:26:15 -0600

    startin fresh

--- docker/postgres/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run -t -i -P -d \
-    --name dragon_pg \
-    -e POSTGRES_DB=drag \
-    -e POSTGRES_USER=drag \
-    -e POSTGRES_PASSWORD=password \
-    -p 5432:5432 \
-    postgres:10.3
--- server/api/short_uuid.py
+++
@@ -1,6 +0,0 @@
-import uuid
-
-
-def short_uuid():
-    u = getattr(uuid, 'uuid1')()
-    return str(u)
