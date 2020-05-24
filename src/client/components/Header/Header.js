0b32b0dc5734828d88da4f547e4c1b1fa705df70
Cyrille Gindreau <cyrillegin@gmail.com>
2020-05-23 19:01:00 -0600

    header outline

--- src/client/components/Header/Header.js
+++ src/client/components/Header/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 
-const Header = () => {
-  console.info('header');
-  return <div>header</div>;
+const Header = ({ className }) => (
+  <div className={className}>
+    <div className="title">Dragonfly</div>
+    <div className="time-pickers">
+      Start
+      <input type="date" />
+      Endasdfasdf
+      <input type="data" />
+    </div>
+  </div>
+);
+Header.propTypes = {
+  className: PropTypes.string.isRequired,
 };
 
-const styledHeader = styled(Header)``;
+const styledHeader = styled(Header)`
+  .title {
+  }
+
+  .time-pickers {
+  }
+`;
 
 export default styledHeader;
