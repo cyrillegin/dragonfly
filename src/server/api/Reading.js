3f060aacc2dbc0ee6fba57ab36f6ae5d0889e753
Cyrille Gindreau <cyrillegin@gmail.com>
2020-05-24 13:54:52 -0600

    fill out reading api

--- src/server/api/Reading.js
+++ src/server/api/Reading.js
@@ -2,14 +2,105 @@ import { Router } from 'express';
 
 const router = new Router();
 
+/**
+ * GET
+ * Gets an array of all the readings for a particular sensor
+ *
+ * kwargs: sensorId (int)
+ *         startTime (date) - default 7 days ago
+ *         endTime (date) - default now
+ *
+ * returns {
+ *   readings: [{
+ *     value: 123,
+ *     timestamp: '10-4'
+ *   }]
+ * }
+ */
 router.get('/', async (req, res) => {
-  console.info('get reading');
-  res.sendStatus(200);
+  console.info('GET request to reading');
+  let {sensorId, startTime, endTime} = req.query;
+  if (!sensorId) {
+    res
+      .status(400)
+      .send({ error: 'You must provide a sensor id' });
+    return;
+  }
+
+  const currentDate = new Date();
+  const tempDate = new Date();
+
+  if (!startTime) {
+    tempDate.setDate(currentDate.getDate() - 7)
+    startTime = tempDate;
+  }
+
+  if (!endTime) {
+    endTime = currentDate;
+  }
+
+  if (endTime < startTime) {
+    tempDate.setDate((new Date(endTime)).getDate() - 7)
+    startTime = tempDate;
+  }
+
+  const readings = await Reading.findAll({
+    where: {
+      sensorId,
+      from: {
+        $between: [startTime, endTime]
+      }
+    }
+  });
+  res.send(stations)
 });
 
+/**
+ * POST
+ * Creates a new station
+ *
+ * params: {
+ *   name: 'station name',
+ *   ip: 'xxx.xxx.xxx.xxx'
+ * }
+ *
+ * returns: 200 success, 400 if validation fails
+ */
 router.post('/', async (req, res) => {
-  console.info('post reading');
-  res.sendStatus(200);
+  console.info('POST request to reading');
+  const { value, timestamp, sensorId, stationId } = req.body;
+
+  const valid = validateReadingParams(req.body);
+  if (valid.error) {
+    res.status(400).send({ error: valid.error });
+    return;
+  }
+
+  try {
+    const result = await Reading.create({ value, timestamp, sensorId, stationId });
+    console.info('new reading added');
+    res.status(200).send({ message: 'success' });
+  } catch (e) {
+    console.error('an error occured!');
+    console.error(e);
+    res.status(400).send({ error: 'An unknown error has occured' });
+  }
 });
+
+const validateReadingParams = params => {
+  if(!params.stationId) {
+    return { error: 'Station id required };
+  }
+  if(!params.sensorId) {
+    return { error: 'Sensor id required };
+  }
+  if(!params.timestamp) {
+    return { error: 'Timestamp required };
+  }
+  if(!params.value) {
+    return { error: 'Value required };
+  }
+  return {}
+}
 
 export default router;
