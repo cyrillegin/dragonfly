1e46088049b84a882c4956b522de5efa3a8a5a1e
Cyrille Gindreau <cyrillegin@gmail.com>
2020-12-18 11:17:30 -0700

    update tests

--- utility/setupTests.js
+++ utility/setupTests.js
@@ -2,7 +2,7 @@ import '@babel/polyfill';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as emotion from '@emotion/core';
-import { createSerializer } from 'jest-emotion';
+import { createSerializer } from '@emotion/jest';
 
 configure({ adapter: new Adapter() });
 
