6cc5b538ce6d6de8efa2433e3dd7637da63ea880
cyrille <cyrillegin@gmail.com>
2021-01-31 17:09:38 -0700

    update sensor

--- satellite/pollers/aht20Poller.py
+++ satellite/pollers/aht20Poller.py
@@ -5,11 +5,14 @@ import adafruit_ahtx0
 # Create the sensor object using I2C
 sensor = adafruit_ahtx0.AHTx0(board.I2C())
 
-def GetValues(sensor):
+def GetValues():
     humidity = sensor.relative_humidity
     print('aht20Poller: Humidity is currently: {}'.format(humidity))
     newReading = {
         'timestamp': time.time() * 1000,
          'value': humidity,
     }
+    print(newReading)
     return newReading
+
+GetValues()
