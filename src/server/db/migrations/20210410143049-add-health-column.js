826198fc1df9eb90760883a06bd7760f946aca8e
Cyrille Gindreau <cyrillegin@gmail.com>
2021-09-12 14:21:59 -0600

    fix typo in migrations

--- src/server/db/migrations/20210410143049-add-health-column.js
+++ src/server/db/migrations/20210410143049-add-health-column.js
@@ -1,15 +1,15 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     Promise.all([
-      queryInterface.addColumn('station', 'last_health_timestamp', Sequelize.DATE),
-      queryInterface.addColumn('sensor', 'last_health_timestamp', Sequelize.DATE),
+      queryInterface.addColumn('stations', 'last_health_timestamp', Sequelize.DATE),
+      queryInterface.addColumn('sensors', 'last_health_timestamp', Sequelize.DATE),
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
     Promise.all([
-      queryInterface.removeColumn('station', 'last_health_timestamp'),
-      queryInterface.removeColumn('sensor', 'last_health_timestamp'),
+      queryInterface.removeColumn('stations', 'last_health_timestamp'),
+      queryInterface.removeColumn('sensors', 'last_health_timestamp'),
     ]);
   },
 };
